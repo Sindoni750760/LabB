4e016c5ed952e1475e3822a75acc951996e4a44d
GioIs <giova.giova90@hotmail.it>
2025-07-14 21:30:00 +0200

    Update User.java

--- theknife-client/src/main/java/com/theknife/app/User.java
+++ theknife-client/src/main/java/com/theknife/app/User.java
@@ -1,1 +1,38 @@
+package com.theknife.app;
 
+import java.io.IOException;
+
+public class User {
+    private static String name, surname;
+    private static boolean logged_in = false, is_restaurateur;
+
+    public static String login(String username, String password) throws IOException {        
+        Communicator.sendStream("login");
+        Communicator.sendStream(username);
+        Communicator.sendStream(password);
+
+        String response = Communicator.readStream();
+
+        if(response.equals("ok")) {
+            Communicator.sendStream("getUserInfo");
+            name = Communicator.readStream();
+            surname = Communicator.readStream();
+            is_restaurateur = Communicator.readStream().equals("y");
+            logged_in = true;
+        }
+
+        return response;
+    }
+
+    public static void logout() throws IOException {
+        Communicator.sendStream("logout");
+        if(Communicator.readStream().equals("ok"))
+            logged_in = false;
+    }
+
+    public static String[] getInfo() {
+        if(!logged_in)
+            return null;
+        return new String[]{name, surname, is_restaurateur ? "y" : "n"};
+    }
+}
