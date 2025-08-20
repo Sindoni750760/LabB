9d1576aaf141dc93e5d5f65a375911a6d0d42bb4
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-08-20 18:15:12 +0200

    Aggiunto il controllo della data di nascita sugli utenti

--- theknife_server/src/main/java/com/theknife/app/User.java
+++ theknife_server/src/main/java/com/theknife/app/User.java
@@ -3,6 +3,7 @@ package com.theknife.app;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -28,7 +29,7 @@ public class User {
     private static boolean checkPassword(String password) {
         if(password.length() < 8 || password.length() > 32)
             return false;
-        
+
         boolean lalph, ualph, num, spec;
         lalph = ualph = num = spec = false;
 
@@ -46,14 +47,14 @@ public class User {
         return lalph && ualph && num && spec;
     }
 
-    public static String registerUser(String nome, String cognome, String username, String password, String data_nascita, String latitude, String longitude, boolean is_ristoratore) throws SQLException {
+    public static String registerUser(String nome, String cognome, String username, String password, String data_nascita, boolean is_ristoratore) throws SQLException {
         //checks missing parameters
         if(nome.trim().isEmpty() || cognome.trim().isEmpty() || username.trim().isEmpty())
             return "missing";
 
         if(!checkPassword(password))
             return "password";
-        
+
         //handles birth date
         long time = -1;
         try {
@@ -66,16 +67,8 @@ public class User {
         //is the birth date is after today, returns the "date" error
         if(time > new java.util.Date().getTime())
             return "date";
-        
-        double latitude_double, longitude_double;
-        try {
-            latitude_double = Double.parseDouble(latitude);
-            longitude_double = Double.parseDouble(longitude);
-        } catch(NumberFormatException e) {
-            return "coordinates";
-        }
 
-        return DBHandler.addUser(nome, cognome, username, hashPassword(password), time, latitude_double, longitude_double, is_ristoratore) ? "ok" : "username";
+        return DBHandler.addUser(nome, cognome, username, hashPassword(password), time, is_ristoratore) ? "ok" : "username";
     }
 
     public static int loginUser(String username, String password) throws SQLException {
@@ -84,10 +77,10 @@ public class User {
         //if no user was found with given username
         if(user_info == null)
             return -1;
-        
+
         if(verifyPassword(password, user_info[1]))
             return Integer.parseInt(user_info[0]);
-        
+
         //password missmatch
         return -2;
     }
