488a68c402f445040aac678236a71fce8eab07b2
Sind01 <102220572+Sindoni750760@users.noreply.github.com>
2025-09-24 16:04:14 +0200

    Inserita documentazione del package e aggiornato il POM

---
+++ src/theknife_client/src/main/java/com/theknife/app/controllers/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package per i controller dell'applicazione client.
+ * <p>
+ * Gestiscce la logica di presentazione e il coordinamento dell'interfaccia utente
+ * e i servizi di backend.
+ * Include:
+ * - la gestione delle recensioni
+ * - la modifica dei ristoranti
+ */
+
+package com.theknife.app.controllers;
---
+++ src/theknife_client/src/main/java/com/theknife/app/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Package principale dell'applicazione client TheKnife.
+ * <p>
+ * Contiene le classi core per il funzionamento del client, inclusa:
+ * - la gestione delle scene,
+ * - la comunicazione con il server,
+ * - l'avvio dell'applicazione
+ * </p>
+ */
+
+package com.theknife.app;
